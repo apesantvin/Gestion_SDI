a50aa4a9ec430fce480ae0431e8e714d6f47486f
apesantvin <55583198+apesantvin@users.noreply.github.com>
2019-09-27 13:44:58 +0200

    Update consulta_banco.py

--- practica2/consulta_banco.py
+++ practica2/consulta_banco.py
@@ -1,22 +1,27 @@
 from urllib.request import urlopen
 import json
-import fileinput
-
-"""Esta clase implementa la conexión a la pagina web, además de transformar la moneda. """
 class ExchangeAPIClient:
-    def  __init__(self):
-        self.bbdd=[]
+
+    def __init__(self):
+        self.datos_web=[]
+        
     def get_datos(self):
         api_url = 'https://api.exchangeratesapi.io/latest'
         response = urlopen(api_url)
-        self.bbdd=json.loads(response.read())
-
+        self.datos_web=json.loads(response.read())
+        
     def convert(self,cantidad, de):
-        return cantidad/self.bbdd['rates'][de]
+        return cantidad/self.datos_web['rates'][de]
+    def get_rates(self):
+        return self.datos_web['rates']
+    def get_date(self):
+        return self.datos_web['date']
+    def get_base(self):
+        return self.datos_web['base']
 
 def escribir_datos(saldo,fecha):
     salida=open("ahorros.txt",'a+')
-    salida.write('{0}, {1} \n'.format(fecha,int(saldo)))
+    salida.write('{}, {} \n'.format(fecha,int(saldo)))
     salida.close()
     
 def leer_datos():
@@ -29,11 +34,13 @@ def leer_datos():
 if __name__== '__main__':
     bbdd_web=ExchangeAPIClient()
     bbdd_web.get_datos()
+    divisas=bbdd_web.get_rates()
+    base=bbdd_web.get_base()
     cantidad_total=0
     datos=leer_datos()
     for element in datos:
-        if element[0] in bbdd_web.bbdd['rates']:
+        if element[0] in divisas:
             cantidad_total = cantidad_total+bbdd_web.convert(int(element[1]),element[0])
-        elif element[0]==bbdd_web.bbdd['base']:
+        elif element[0]==base:
                 cantidad_total = cantidad_total+int(element[1])
-    escribir_datos(cantidad_total,bbdd_web.bbdd['date'])
+    escribir_datos(cantidad_total,bbdd_web.get_date())
